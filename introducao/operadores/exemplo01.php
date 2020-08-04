96f530c131388b881f95ea6dbe27c8fad3470906
Alessandro <alessandroadm@live.com>
2020-08-04 01:58:46 -0300

    adding exemple 03

---
+++ introducao/operadores/exemplo01.php
@@ -0,0 +1,28 @@
+<?php
+//Atribuição =
+$nome = "Alessandro";
+$sobrenome = "Menezes";
+
+//Concatenação . 
+$nomeCompleto = $nome . " " . $sobrenome;
+
+//Concatenação e atribuição .=
+$nomeCompleto .= " Está estudando PHP";
+
+//Soma incrementada
+$valorTotal = 0;
+$valorTotal += 100;
+$valorTotal +=25;
+
+echo($valorTotal);
+
+$a = 10;
+$b = 5;
+
+$soma = $a + $b;
+$divisao = $a / $b;
+$subtracao = $a - $b;
+$multiplicacao = $a * $b;
+$modulo = $a % $b;
+
+?>
---
+++ introducao/operadores/exemplo02.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 10;
+$b = 50;
+
+$menor = $a < $b;
+$maior = $a > $b;
+$menorOuIgual = $a <= $b;
+$maiorOuIgual = $a >= $b;
+$comparacaoIgualValor = $a == $b;
+$comparacaoIgualValorTipo = $a === $b;
+$valorDiferente = $a != $b;
+$valorTipoDiferente = $a !== $b;
+
+$valorMaiorMenorOuIgual = $a <=> $b;
+
+?>
