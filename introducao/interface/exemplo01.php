87928d03245acf494da77cc19ce49e4fdd6312f2
Alessandro <alessandroadm@live.com>
2020-08-26 00:59:01 -0300

    doc(interface): adding interface exemple

---
+++ introducao/interface/exemplo01.php
@@ -0,0 +1,29 @@
+<?php
+
+interface Veiculo{
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+}
+
+
+class Civic implements Veiculo{
+
+    public function acelerar($velocidade){
+        echo "O veiculo acelerou ate " . $velocidade ." km/h";
+    }
+    public function frenar($velocidade){
+        echo "O veiculo frebou até " . $velocidade ." km/h";
+    }
+    public function trocarMarcha($marcha){
+        echo "O veiculo engatou a marcha " . $marcha;
+    }
+
+}
+
+$carro = new Civic();
+$carro->trocarMarcha(3);
+
+var_dump($carro);
+
+?>
